c476c1a1ea669da83fb82f190bff44ecbbd94e7f
Igor Lopes <claudioigorai@gmail.com>
2022-09-26 11:11:12 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello word! x" + (a+b));*/
+		
+		Gato gato = new Gato(null, null, null);
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+}
+
+class Livros{
+	private String nome;
+	private String npag;
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Igor
+ *
+ */
+module primeiroprograma.java {
+}
